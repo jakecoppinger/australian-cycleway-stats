48794e1252e01e96692ef7489fabc6cba75dd895
Jake Coppinger <jake@jakecoppinger.com>
2024-11-09 11:34:57 +1100

    Call Overpass API one more time if the first attempt flakes

--- static-backend/src/api/overpass.ts
+++ static-backend/src/api/overpass.ts
@@ -4,9 +4,27 @@ import { cachedFunctionCall } from "../utils/cached-function-call.js";
 
 export async function cachedOverpassTurboRequest({ request, overpassEndpoint }: 
   { request: string, overpassEndpoint: string }): Promise<(OSMNode | OSMWay | OSMRelation)[]> {
-  return cachedFunctionCall({request, overpassEndpoint}, overpassTurboRequest);
+  return cachedFunctionCall({request, overpassEndpoint}, robustOverpassTurboRequest);
 }
 
+/**
+ * Repeatedly call `overpassTurboRequest` up to `maxTries` times until it succeeds.
+ */
+export async function robustOverpassTurboRequest({ request, overpassEndpoint, maxTries=2 }: { request: string, overpassEndpoint: string, maxTries?: number }): Promise<(OSMNode | OSMWay | OSMRelation)[]> {
+  let tries = 0;
+  while (true) {
+    try {
+      return await overpassTurboRequest({ request, overpassEndpoint });
+    } catch (e) {
+      tries++;
+      if (tries >= maxTries) {
+        console.error(`Failed to fetch data after ${maxTries} attempts.`);
+        throw e;
+      }
+      console.error(`Failed to fetch data on attempt ${tries} of ${maxTries}, retrying...`);
+    }
+  }
+}
 
 export async function overpassTurboRequest({ request, overpassEndpoint }: { request: string, overpassEndpoint: string }): Promise<(OSMNode | OSMWay | OSMRelation)[]> {
   const apiUrl = overpassEndpoint ? overpassEndpoint : config.overpassApiEndpoints.default
